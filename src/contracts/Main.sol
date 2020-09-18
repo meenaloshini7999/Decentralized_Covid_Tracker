d4d99f2d062ff211bece76e59eb5fa2565bb45e8
sivasubramanian8055 <yosiva137@gmail.com>
2020-09-19 02:57:34 +0530

    First Commit

---
+++ src/contracts/Main.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+contract Main {
+  address public admin;
+  struct localAdmin{
+    address ladminadd;
+    uint aadhar;
+    uint pincode;
+  }
+  struct Doctor{
+    address docAdd;
+    string id;
+    string workPlace;
+  }
+  struct flat{
+    
+  }
+
+}
